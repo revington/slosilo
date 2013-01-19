21f8cb8412d99884bbdea332f37d377d0a68e57a
Pedro Narciso García Revington <p.revington@gmail.com>
2013-01-19 19:20:59 +0100

    route for project

--- routes/project.js
+++ routes/project.js
@@ -1,8 +1,27 @@
-module.exports.view = function (req, res) {
-    res.render('project', {
-        title: 'loading project'
+function loadProject(req, res, next) {
+    req.app.get('db').get(req.params.uuid, function (err, data) {
+        if (err) {
+            console.error(err);
+        }
+        res.locals.data = data;
+        return next();
     });
-};
+}
+module.exports.view = [function (req, res, next) {
+    res.format({
+        html: function () {
+            res.render('project', {
+                title: 'loading project'
+            });
+        },
+        json: function () {
+            next();
+        }
+    });
+},
+loadProject, function (req, res) {
+    res.json(res.locals.data);
+}];
 module.exports.update = function (req, res) {
     res.redirect('/project/' + req.params.uuid);
 };
