c3278cb9ca6995fe928f18e998603f59c35a30ce
Pedro Narciso García Revington <p.revington@gmail.com>
2013-01-30 21:39:52 +0100

    client protoype working

--- public/js/project.js
+++ public/js/project.js
@@ -20,34 +20,66 @@
             }
             var f = function (item) {
                     return item.date.getMonth() + 1;
-                }, c = f(items[0]),
-                i, j, ret = [], epoch;
-								epoch = items[0].date;
-            for (j = 0, i = 0; i < items.length; i++) {
-              if (f(items[i]) !== c) {
+                },
+                c = f(items[0]),
+                i, j, ret = [],
+                len = items.length,
+                epoch;
+            epoch = items[0].date;
+            for (j = 0, i = 0; i < len; i++) {
+                if (f(items[i]) !== c) {
                     ret.push({
-											time: epoch,
+                        time: epoch,
                         days: items.slice(j, i)
                     });
-								epoch = items[i].date;
+                    epoch = items[i].date;
                     c = f(items[i]);
                     j = i;
                 }
             }
             ret.push({
-							time: epoch,
+                time: epoch,
                 days: items.slice(j, i)
             });
             return ret;
         },
+        monthArray: function () {
+            var ret = [],
+                i, monthCollection = 'JFMAMJJASOND';
+            for (i = 0; i < monthCollection.length; i++) {
+                ret.push({
+                    value: i,
+                    text: monthCollection[i]
+                });
+            }
+            return ret;
+        },
+        timeWindowValues: function () {
+            var i, ret = [];
+            for (i = 1; i < 12; i++) {
+                ret.push({
+                    text: i + ' month',
+                    value: i
+                });
+            }
+            ret.push({
+                text: 'one year',
+                value: 12
+            });
+            ret.push({
+                text: 'two year',
+                value: 24
+            });
+            return ret;
+        },
         calendar: function () {
-            var from = this.showFrom(),
-                to = this.showTo(),
+            var from = this.from().getTime(),
+                to = this.to().getTime(),
                 items = this.changes(),
                 findFirst = MODELS.PROJECT.reverseSearchForValue(items),
                 ret = [],
                 dayTicks = MODELS.PROJECT.dayTicks;
-            for (; from <= to; from += dayTicks) {
+            for (; from < to; from += dayTicks) {
                 ret.push({
                     value: findFirst(from),
                     date: new Date(from)
@@ -56,14 +88,37 @@
             return MODELS.PROJECT.groupByMonth(ret);
         },
         extendProject: function (that) {
-            var cal = this.calendar;
-            that.showFrom = ko.observable(new Date().getTime());
-            that.showTo = ko.observable(new Date().getTime() + (5 * 30 * 24 * 60 * 60 * 1000));
+            var cal = this.calendar,
+                now = new Date(),
+                i, nowMonth = now.getMonth();
+            // years
+            that.fromYearValues = ko.observableArray([now.getFullYear() - 1, now.getFullYear(), now.getFullYear() + 1]);
+            that.selectedYear = ko.observable(that.fromYearValues()[1]);
+            // month
+            that.fromMonthValues = ko.observableArray(MODELS.PROJECT.monthArray());
+            that.selectedMonth = ko.observable(that.fromMonthValues()[nowMonth]);
+            that.setSelectedMonth = function (val) {
+                that.selectedMonth(val);
+            };
+            // from
+            that.from = ko.computed(function () {
+                var ret = new Date(that.selectedYear(), that.selectedMonth().value, 1);
+                return ret;
+            });
+            // time window
+            that.timeWindow = ko.observableArray(MODELS.PROJECT.timeWindowValues());
+            that.selectedTimeWindow = ko.observable(that.timeWindow()[4]);
+            // to
+            that.to = ko.computed(function () {
+                var ret = new Date(that.from().getTime()),
+                    myWindow = that.selectedTimeWindow().value;
+                ret.setMonth(ret.getMonth() + myWindow);
+                return ret;
+            });
             that.calendar = ko.computed(function () {
                 return cal.call(that);
             });
-            console.log(new Date(that.showFrom()));
-            console.log(new Date(that.showTo()));
+            that.monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
         }
     };
 })(window.MODELS);
